31ef9e9bbe628d5bfbc94b2e7a95c92b2ede2b23
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-08 01:08:56 -0300

    Iniciada a aplicação.

---
+++ forum/src/main/java/br/com/guifroes1984/forum/ForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
