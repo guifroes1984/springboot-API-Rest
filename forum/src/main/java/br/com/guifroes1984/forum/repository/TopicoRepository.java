68de044ea26cac8ae801dea2dc7c319fa9c18e55
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-09 13:45:41 -0300

    Consulta com filtros.

--- forum/src/main/java/br/com/guifroes1984/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/guifroes1984/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.guifroes1984.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.guifroes1984.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+	
+	List<Topico> findByCursoNome(String nomeCurso);
 
 }
