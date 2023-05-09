699e67c695b7724f31fd4cfae23c53aa79140454
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-09 09:51:59 -0300

    Criado o pacote de modelo.

---
+++ forum/src/main/java/br/com/guifroes1984/forum/modelo/Curso.java
@@ -0,0 +1,52 @@
+package br.com.guifroes1984.forum.modelo;
+
+import java.util.Objects;
+
+public class Curso {
+
+	private Long id;
+	private String nome;
+	private String categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ forum/src/main/java/br/com/guifroes1984/forum/modelo/StatusTopico.java
@@ -0,0 +1,9 @@
+package br.com.guifroes1984.forum.modelo;
+
+public enum StatusTopico {
+
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+}
