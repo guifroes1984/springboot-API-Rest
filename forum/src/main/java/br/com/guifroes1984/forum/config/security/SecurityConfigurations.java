9d796e6bfefd4093a0f1435b861d99e716c5191a
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-23 11:30:08 -0300

    Habilitando o Spring Security.

---
+++ forum/src/main/java/br/com/guifroes1984/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,12 @@
+package br.com.guifroes1984.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	
+}
