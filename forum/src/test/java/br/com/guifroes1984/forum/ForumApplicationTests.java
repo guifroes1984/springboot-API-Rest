94b8f0271a8403d9704c11d49331b06ec0357f77
guifroes1984 <guilhermefroes26@gmail.com>
2023-05-26 15:40:50 -0300

    Testando um repository com JUnit.

--- forum/src/test/java/br/com/guifroes1984/forum/ForumApplicationTests.java
+++ forum/src/test/java/br/com/guifroes1984/forum/ForumApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ForumApplicationTests {
+public class ForumApplicationTests {
 
 	@Test
 	public void contextLoads() {
